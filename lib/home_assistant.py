6225cacca68b31fd8660ef5cc946fc170ab05ae8
Rob Weber <robweberjr@gmail.com>
2021-12-28 15:56:39 -0600

    part of #2

--- lib/home_assistant.py
+++ lib/home_assistant.py
@@ -1,11 +1,21 @@
 import requests
 import json
 
+
 class HomeAssistant:
+    """
+    The HomeAssistant class is a simple class for the purposes
+    of getting state and attribute information for different \
+    entities from Home Assistant.
+    """
     url = None
     token = None
 
     def __init__(self, url, token):
+        """
+        url is the fill url to an HA instance starting with http:// or http://
+        token is a long lived access token created in HA
+        """
         self.url = url
         self.token = token
 
@@ -15,12 +25,12 @@ class HomeAssistant:
             'content-type': 'application/json',
         }
 
-        response = requests.get('%s%s' % (self.url,endpoint), headers=headers)
+        response = requests.get('%s%s' % (self.url, endpoint), headers=headers)
 
         return json.loads(response.text)
 
-    def getStates(self, entity = ''):
+    def getStates(self, entity=''):
         return self._makeRequest('/api/states/%s' % entity)
 
-    def getState(self, entity = ''):
+    def getState(self, entity=''):
         return self.getStates(entity)
